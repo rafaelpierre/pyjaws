0be33d6f0c757ab47dbdba79d5a6520e4f809b33
Ajith Aravind <ajith.aravind100@gmail.com>
2023-05-15 23:25:20 +0530

    test: :white_check_mark: add notebook & spark python task to fixture

--- tests/fixtures/job.py
+++ tests/fixtures/job.py
@@ -1,8 +1,9 @@
-from pyjaws.api.base import (
-    Cluster,
-    Task,
-    Runtime,
-    Workflow
+from pyjaws.api.base import Cluster, Runtime, Workflow
+from pyjaws.api.tasks import (
+    PythonWheelTask,
+    NotebookTask,
+    SparkPythonTask,
+    Source,
 )
 
 import pytest
@@ -10,52 +11,75 @@ import pytest
 
 @pytest.fixture
 def workflow_fixture() -> Workflow:
-
     cluster = Cluster(
-        job_cluster_key = "mycluster",
-        spark_version = Runtime.DBR_13_ML,
-        node_type_id = "Standard_E4ds_v4",
-        num_workers = 3
+        job_cluster_key="mycluster",
+        spark_version=Runtime.DBR_13_ML,
+        node_type_id="Standard_E4ds_v4",
+        num_workers=3,
     )
 
-    ingest_task = Task(
-        key = "ingest",
-        cluster = cluster,
-        entrypoint = "iot",
-        package_name = "deepspeed",
-        task_name = "ingest",
-        parameters = [
+    ingest_task = PythonWheelTask(
+        key="ingest",
+        cluster=cluster,
+        entrypoint="iot",
+        package_name="deepspeed",
+        task_name="ingest",
+        parameters=[
             f"dummy.parquet",
-            "--output-db", "sensors",
-            "--output-table", "bronze",
-            "--zorder-column", "TagName",
-            "--write-mode", "append"
-        ]
+            "--output-db",
+            "sensors",
+            "--output-table",
+            "bronze",
+            "--zorder-column",
+            "TagName",
+            "--write-mode",
+            "append",
+        ],
     )
 
-    aggregate_task = Task(
-        key = "aggregate",
-        cluster = cluster,
-        entrypoint = "iot",
-        libraries = [{"pypi": "deepspeed"}],
-        package_name = "deepspeed",
-        task_name = "aggregate",
-        dependencies = [ingest_task],
-        parameters = [
-            "--input-db", "sensors",
-            "--input-table", "bronze",
-            "--output-db", "sensors",
-            "--output-table", "silver",
-            "--zorder-column", "TagName"
-        ]
+    aggregate_task = PythonWheelTask(
+        key="aggregate",
+        cluster=cluster,
+        entrypoint="iot",
+        libraries=[{"pypi": "deepspeed"}],
+        package_name="deepspeed",
+        task_name="aggregate",
+        dependencies=[ingest_task],
+        parameters=[
+            "--input-db",
+            "sensors",
+            "--input-table",
+            "bronze",
+            "--output-db",
+            "sensors",
+            "--output-table",
+            "silver",
+            "--zorder-column",
+            "TagName",
+        ],
+    )
+
+    visualizations_notebook = NotebookTask(
+        key="visualizations",
+        cluster=cluster,
+        dependencies=[aggregate_task],
+        libraries=[{"pypi": "plotly"}],
+        notebook_path="/Workspace/Shared/visualizations",
+        source=Source.WORKSPACE,
+    )
+
+    cleanup_task = SparkPythonTask(
+        key="cleanup",
+        cluster=cluster,
+        dependencies=[visualizations_notebook],
+        python_file="/Workspace/Repos/bob@mail.com/utils/cleanup.py",
+        source=Source.WORKSPACE,
     )
 
     workflow = Workflow(
-        name = "test_workflow_iot",
-        tasks = [ingest_task, aggregate_task],
-        tags = {
-            "test": "value"
-        }
+        name="test_workflow_iot",
+        tasks=[ingest_task, aggregate_task, visualizations_notebook, cleanup_task],
+        tags={"test": "value"},
     )
 
     return workflow
@@ -63,33 +87,34 @@ def workflow_fixture() -> Workflow:
 
 @pytest.fixture
 def workflow_fixture_no_tags() -> Workflow:
-
     cluster = Cluster(
-        job_cluster_key = "mycluster",
-        spark_version = Runtime.DBR_13_ML,
-        node_type_id = "Standard_E4ds_v4",
-        num_workers = 3
+        job_cluster_key="mycluster",
+        spark_version=Runtime.DBR_13_ML,
+        node_type_id="Standard_E4ds_v4",
+        num_workers=3,
     )
 
-    aggregate_task = Task(
-        key = "aggregate",
-        cluster = cluster,
-        entrypoint = "iot",
-        package_name = "deepspeed",
-        libraries = [{"pypi": "deepspeed"}],
-        task_name = "aggregate",
-        parameters = [
-            "--input-db", "sensors",
-            "--input-table", "bronze",
-            "--output-db", "sensors",
-            "--output-table", "silver",
-            "--zorder-column", "TagName"
-        ]
+    aggregate_task = PythonWheelTask(
+        key="aggregate",
+        cluster=cluster,
+        entrypoint="iot",
+        package_name="deepspeed",
+        libraries=[{"pypi": "deepspeed"}],
+        task_name="aggregate",
+        parameters=[
+            "--input-db",
+            "sensors",
+            "--input-table",
+            "bronze",
+            "--output-db",
+            "sensors",
+            "--output-table",
+            "silver",
+            "--zorder-column",
+            "TagName",
+        ],
     )
 
-    workflow = Workflow(
-        name = "test_workflow_iot",
-        tasks = [aggregate_task]
-    )
+    workflow = Workflow(name="test_workflow_iot", tasks=[aggregate_task])
 
     return workflow
