b3050734310dad0c1148e2cfd74040fc551f35bf
Ajith Aravind <ajith.aravind100@gmail.com>
2023-05-15 23:25:40 +0530

    docs: :memo: update task type in readme & examples

--- examples/simple_workflow/workflows.py
+++ examples/simple_workflow/workflows.py
@@ -1,63 +1,45 @@
-from pyjaws.api.base import (
-    Cluster,
-    Runtime,
-    Task,
-    Workflow
-)
+from pyjaws.api.base import Cluster, Runtime, Workflow
+from pyjaws.api.tasks import PythonWheelTask
 
 cluster = Cluster(
-    job_cluster_key = "ai_cluster",
-    spark_version = Runtime.DBR_13_ML,
-    num_workers = 2,
-    node_type_id = "Standard_DS3_v2",
-    cluster_log_conf = {
-        "dbfs": {
-            "destination": "dbfs:/home/cluster_log"
-        }
-    }
+    job_cluster_key="ai_cluster",
+    spark_version=Runtime.DBR_13_ML,
+    num_workers=2,
+    node_type_id="Standard_DS3_v2",
+    cluster_log_conf={"dbfs": {"destination": "dbfs:/home/cluster_log"}},
 )
 
 # Create a Task object.
 
-ingest_task = Task(
-    key = "ingest",
-    cluster = cluster,
-    package_name = "my_package",
-    entrypoint = "ingest",
-    task_name = "ingest",
-    libraries = [
-        {
-            "pypi": {
-                "package": "deepspeed"
-            }
-        }
-    ],
-    parameters = [
-        "my_parameter_value",
-        "--output-table", "my_table"
-    ]
+ingest_task = PythonWheelTask(
+    key="ingest",
+    cluster=cluster,
+    package_name="my_package",
+    entrypoint="ingest",
+    task_name="ingest",
+    libraries=[{"pypi": {"package": "deepspeed"}}],
+    parameters=["my_parameter_value", "--output-table", "my_table"],
 )
 
-transform_task = Task(
-    key = "transform",
-    cluster = cluster,
-    package_name = "deepspeed",
-    entrypoint = "deepspeed",
-    task_name = "ingest",
-    dependencies = [ingest_task],
-    libraries = [{"pypi": "deepspeed"}],
-    parameters = [
-       "--num_nodes", "2",
-       "--num_gpus", "2",
-       "run_glue_classifier_bert_base.py"
-    ]
+transform_task = PythonWheelTask(
+    key="transform",
+    cluster=cluster,
+    package_name="deepspeed",
+    entrypoint="deepspeed",
+    task_name="ingest",
+    dependencies=[ingest_task],
+    libraries=[{"pypi": "deepspeed"}],
+    parameters=[
+        "--num_nodes",
+        "2",
+        "--num_gpus",
+        "2",
+        "run_glue_classifier_bert_base.py",
+    ],
 )
 
 
 # Create a Workflow object to define dependencies
 # between previously defined tasks.
 
-workflow = Workflow(
-    name = "my_workflow",
-    tasks = [ingest_task, transform_task]
-)
+workflow = Workflow(name="my_workflow", tasks=[ingest_task, transform_task])
