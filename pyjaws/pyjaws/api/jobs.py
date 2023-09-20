6dd7f7cfb0b553e618b04ed0ecf53d752068b5fd
Rafael Pierre <rafael.pierre@databricks.com>
2023-09-20 15:43:23 +0200

    lint

--- pyjaws/pyjaws/api/jobs.py
+++ pyjaws/pyjaws/api/jobs.py
@@ -21,15 +21,22 @@ def create(workflow: Workflow, overwrite: bool = False):
 
     try:
         api_client = ApiClient()
-        logging.info(f"Deploying jobs...")
-        result = None
+        logging.info("Deploying jobs...")
         jobs_api = JobsAPI(api_client)
         existing_jobs = list(jobs_api.list(name=workflow.name))
         dict_settings = workflow.json()
-        settings = JobSettings(**dict_settings)
+        JobSettings(**dict_settings)
         logging.info(f"Found existing jobs: {existing_jobs}")
-        dict_settings["job_clusters"] = [JobCluster.from_dict(cluster) for cluster in dict_settings["job_clusters"]]
-        dict_settings["tasks"] = [Task.from_dict(task) for task in dict_settings["tasks"]]
+        dict_settings["job_clusters"] = [
+            JobCluster.from_dict(cluster)
+            for cluster
+            in dict_settings["job_clusters"]
+        ]
+        dict_settings["tasks"] = [
+            Task.from_dict(task)
+            for task
+            in dict_settings["tasks"]
+        ]
 
         if overwrite and len(existing_jobs) > 0:
             for job in existing_jobs: 
