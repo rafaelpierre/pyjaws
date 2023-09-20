d6a53369e7b9ddfa3eb26c805d2081c47c6d4956
Rafael Pierre <rafael.pierre@databricks.com>
2023-09-20 16:52:48 +0200

    docs

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+from pallets_sphinx_themes import get_version
+from pallets_sphinx_themes import ProjectLink
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../pyjaws/pyjaws'))
+
+# Project --------------------------------------------------------------
+
+project = "pyjaws"
+author = "pyjaws"
+release, version = get_version("pyjaws")
+
+# General --------------------------------------------------------------
+
+master_doc = "index"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.log_cabinet",
+    "pallets_sphinx_themes",
+    "sphinx_issues",
+    "sphinx_tabs.tabs",
+    "sphinx.ext.napoleon",
+    "myst_parser"
+]
+autodoc_typehints = "description"
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
+
+# HTML -----------------------------------------------------------------
+
+html_theme = "click"
+html_theme_options = {"index_sidebar_logo": False}
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html", "ethicalads.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html", "ethicalads.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html", "ethicalads.html"]}
+html_static_path = ["_static"]
+html_favicon = "_static/click-icon.png"
+html_logo = "_static/click-logo-sidebar.png"
+html_title = f"PyJaws Documentation ({version})"
+html_show_sourcelink = False
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+add_module_names = True
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+🦈 pyjaws
+==================
+
+* **PyJaws** enables declaring Databricks Jobs and Workflows as Python code, allowing for code Linting, Formatting, Parameter Validation, Modularity and reusability.
+* In addition to those, PyJaws also provides some nice features such as **cycle detection** out of the box.
+
+Folks who have used Python-based orchestration tools such as **Apache Airflow**, **Luigi** and **Mage** will be familiar with the concepts and the API if **PyJaws**.
+
+API Reference
+-------------
+
+If you are looking for information on a specific function, class, or
+method, this part of the documentation is for you.
+
+.. toctree::
+   :maxdepth: 2
+
+   api
